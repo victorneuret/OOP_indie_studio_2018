da24f60924f08471971185352fea3a7b158fea4c
Duter991 <lucas.claisse@epitech.eu>
2019-05-17 16:01:42 +0200

    Update AComponent.cpp

--- src/ECS/Abstracts/AComponent.cpp
+++ src/ECS/Abstracts/AComponent.cpp
@@ -7,7 +7,7 @@
 
 #include "ECS/Abstracts/AComponent.hpp"
 
-decltype(Engine::ECS::AComponent::ID) Engine::ECS::AComponent::ID = 0;
+decltype(Engine::ECS::AComponent::ID) Engine::ECS::AComponent::ID{0};
 
 decltype(Engine::ECS::AComponent::_id) Engine::ECS::AComponent::id() const noexcept
 {
