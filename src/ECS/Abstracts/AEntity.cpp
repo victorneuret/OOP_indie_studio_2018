8272f6a04858b0ecb774dc1570fefcb1616c67f4
shellbear <antoine.ordonez@epitech.eu>
2019-05-17 19:58:30 +0200

    Fix indentation

--- src/ECS/Abstracts/AEntity.cpp
+++ src/ECS/Abstracts/AEntity.cpp
@@ -11,12 +11,12 @@
 
 void Engine::ECS::AEntity::addComponent(size_t id)
 {
-	_components.push_back(id);
+    _components.push_back(id);
 }
 
 void Engine::ECS::AEntity::removeComponent(size_t id)
 {
-	_components.erase(std::remove(_components.begin(), _components.end(), id), _components.end());
+    _components.erase(std::remove(_components.begin(), _components.end(), id), _components.end());
 }
 
 const decltype(Engine::ECS::AEntity::_components) &Engine::ECS::AEntity::components() const noexcept
