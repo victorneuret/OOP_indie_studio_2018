0fec409f1ee9de6689d32ce193fb139e237e4047
byFuuRy <yann.sainson@epitech.eu>
2019-05-20 21:02:03 +0200

    Apply requested PR changes

--- Engine/include/Math/Vector/Vec2.hpp
+++ Engine/include/Math/Vector/Vec2.hpp
@@ -7,201 +7,197 @@
 
 #pragma once
 
-namespace Engine::Math {
-	class Vec2;
+namespace Engine::Math
+{
+class Vec2;
 }
 
-template<typename T>
-class Vec2 {
+template <typename T>
+class Vec2
+{
 public:
-	T x{0};
-	T y{0};
+    T x{0};
+    T y{0};
 
-	Vec2<T>() noexcept;
-	Vec2<T>(const Vec2<T> &vec) noexcept;
-	Vec2<T>(const T &x, const T &y) noexcept;
-	~Vec2<T>() = default;
+    Vec2<T>() noexcept;
+    Vec2<T>(const Vec2<T> &vec) noexcept;
+    Vec2<T>(const T x, const T y) noexcept;
+    ~Vec2<T>() = default;
 
-	Vec2<T> &operator+=(const Vec2<T> &vec) noexcept;
-	Vec2<T> &operator+=(const T &val) noexcept;
-	Vec2<T> &operator-=(const Vec2<T> &vec) noexcept;
-	Vec2<T> &operator-=(const T &val) noexcept;
-	Vec2<T> &operator*=(const Vec2<T> &vec) noexcept;
-	Vec2<T> &operator*=(const T &val) noexcept;
-	Vec2<T> &operator/=(const Vec2<T> &vec);
-	Vec2<T> &operator/=(const T &val);
-	Vec2<T> operator+(const Vec2<T> &vec) const noexcept;
-	Vec2<T> operator+(const T &val) const noexcept;
-	Vec2<T> operator-(const Vec2<T> &vec) const noexcept;
-	Vec2<T> operator-(const T &val) const noexcept;
-	Vec2<T> operator*(const Vec2<T> &vec) const noexcept;
-	Vec2<T> operator*(const T &val) const noexcept;
-	Vec2<T> operator/(const Vec2<T> &vec) const;
-	Vec2<T> operator/(const T &val) const;
-	Vec2<T> &operator=(const Vec2<T> &vec) noexcept;
-	Vec2<T> &operator=(const T &val) noexcept;
-	bool operator==(const Vec2<T> &vec) const noexcept;
-	bool operator!=(const Vec2<T> &vec) const noexcept;
+    Vec2<T> &operator+=(const Vec2<T> &vec) noexcept;
+    Vec2<T> &operator+=(const T val) noexcept;
+    Vec2<T> &operator-=(const Vec2<T> &vec) noexcept;
+    Vec2<T> &operator-=(const T val) noexcept;
+    Vec2<T> &operator*=(const Vec2<T> &vec) noexcept;
+    Vec2<T> &operator*=(const T val) noexcept;
+    Vec2<T> &operator/=(const Vec2<T> &vec);
+    Vec2<T> &operator/=(const T val);
+    Vec2<T> operator+(const Vec2<T> &vec) const noexcept;
+    Vec2<T> operator+(const T val) const noexcept;
+    Vec2<T> operator-(const Vec2<T> &vec) const noexcept;
+    Vec2<T> operator-(const T val) const noexcept;
+    Vec2<T> operator*(const Vec2<T> &vec) const noexcept;
+    Vec2<T> operator*(const T val) const noexcept;
+    Vec2<T> operator/(const Vec2<T> &vec) const;
+    Vec2<T> operator/(const T val) const;
+    Vec2<T> &operator=(const Vec2<T> &vec) noexcept;
+    Vec2<T> &operator=(const T val) noexcept;
+    bool operator==(const Vec2<T> &vec) const noexcept;
+    bool operator!=(const Vec2<T> &vec) const noexcept;
 };
 
-template<typename T>
+template <typename T>
 Vec2<T>::Vec2() noexcept
-{
-	x = 0;
-	y = 0;
-}
+{}
 
-template<typename T>
+template <typename T>
 Vec2<T>::Vec2(const Vec2<T> &vec) noexcept
-{
-	x = vec.x;
-	y = vec.y;
-}
+    : x{vec.x}, y{vec.y}
+{}
 
-template<typename T>
-Vec2<T>::Vec2(const T &x, const T &y) noexcept
-{
-	x = x;
-	y = y;
-}
+template <typename T>
+Vec2<T>::Vec2(const T x, const T y) noexcept
+    : x{x}, y{y}
+{}
 
-template<typename T>
+template <typename T>
 Vec2<T> &Vec2<T>::operator+=(const Vec2<T> &vec) noexcept
 {
-	x += vec.x;
-	y += vec.y;
-	return *this;
+    x += vec.x;
+    y += vec.y;
+    return *this;
 }
 
-template<typename T>
-Vec2<T> &Vec2<T>::operator+=(const T &val) noexcept
+template <typename T>
+Vec2<T> &Vec2<T>::operator+=(const T val) noexcept
 {
-	x += val;
-	y += val;
-	return *this;
+    x += val;
+    y += val;
+    return *this;
 }
 
-template<typename T>
+template <typename T>
 Vec2<T> &Vec2<T>::operator-=(const Vec2<T> &vec) noexcept
 {
-	x -= vec.x;
-	y -= vec.y;
-	return *this;
+    x -= vec.x;
+    y -= vec.y;
+    return *this;
 }
 
-template<typename T>
-Vec2<T> &Vec2<T>::operator-=(const T &val) noexcept
+template <typename T>
+Vec2<T> &Vec2<T>::operator-=(const T val) noexcept
 {
-	x -= val;
-	y -= val;
-	return *this;
+    x -= val;
+    y -= val;
+    return *this;
 }
 
-template<typename T>
+template <typename T>
 Vec2<T> &Vec2<T>::operator*=(const Vec2<T> &vec) noexcept
 {
-	x *= vec.x;
-	y *= vec.y;
-	return *this;
+    x *= vec.x;
+    y *= vec.y;
+    return *this;
 }
 
-template<typename T>
-Vec2<T> &Vec2<T>::operator*=(const T &val) noexcept
+template <typename T>
+Vec2<T> &Vec2<T>::operator*=(const T val) noexcept
 {
-	x *= val;
-	y *= val;
-	return *this;
+    x *= val;
+    y *= val;
+    return *this;
 }
 
-template<typename T>
+template <typename T>
 Vec2<T> &Vec2<T>::operator/=(const Vec2<T> &vec)
 {
-	x /= vec.x;
-	y /= vec.y;
-	return *this;
+    x /= vec.x;
+    y /= vec.y;
+    return *this;
 }
 
-template<typename T>
-Vec2<T> &Vec2<T>::operator/=(const T &val)
+template <typename T>
+Vec2<T> &Vec2<T>::operator/=(const T val)
 {
-	x /= val;
-	y /= val;
+    x /= val;
+    y /= val;
+    return *this;
 }
 
-template<typename T>
+template <typename T>
 Vec2<T> Vec2<T>::operator+(const Vec2<T> &vec) const noexcept
 {
-	return Vec2<T>{*this} += vec;
+    return Vec2<T>{x + vec.x, y + vec.y};
 }
 
-template<typename T>
-Vec2<T> Vec2<T>::operator+(const T &val) const noexcept
+template <typename T>
+Vec2<T> Vec2<T>::operator+(const T val) const noexcept
 {
-	return Vec2<T>{*this} += val;
+    return Vec2<T>{x + val, y + val};
 }
 
-template<typename T>
+template <typename T>
 Vec2<T> Vec2<T>::operator-(const Vec2<T> &vec) const noexcept
 {
-	return Vec2<T>{*this} -= vec;
+    return Vec2<T>{x - vec.x, y - vec.y};
 }
 
-template<typename T>
-Vec2<T> Vec2<T>::operator-(const T &val) const noexcept
+template <typename T>
+Vec2<T> Vec2<T>::operator-(const T val) const noexcept
 {
-	return Vec2<T>{*this} -= val;
+    return Vec2<T>{x - val, y - val};
 }
 
-template<typename T>
+template <typename T>
 Vec2<T> Vec2<T>::operator*(const Vec2<T> &vec) const noexcept
 {
-	return Vec2<T>{*this} *= vec;
+    return Vec2<T>{x * vec.x, y * vec.y};
 }
 
-template<typename T>
-Vec2<T> Vec2<T>::operator*(const T &val) const noexcept
+template <typename T>
+Vec2<T> Vec2<T>::operator*(const T val) const noexcept
 {
-	return Vec2<T>{*this} *= val;
+    return Vec2<T>{x * val, y * val};
 }
 
-template<typename T>
+template <typename T>
 Vec2<T> Vec2<T>::operator/(const Vec2<T> &vec) const
 {
-	return Vec2<T>{*this} /= vec;
+    return Vec2<T>{x / vec.x, y / vec.y};
 }
 
-template<typename T>
-Vec2<T> Vec2<T>::operator/(const T &val) const
+template <typename T>
+Vec2<T> Vec2<T>::operator/(const T val) const
 {
-	return Vec2<T>{*this} /= val;
+    return Vec2<T>{x / val, y / val};
 }
 
-template<typename T>
+template <typename T>
 Vec2<T> &Vec2<T>::operator=(const Vec2<T> &vec) noexcept
 {
-	x = vec.x;
-	y = vec.y;
-	return *this;
+    x = vec.x;
+    y = vec.y;
+    return *this;
 }
 
-template<typename T>
-Vec2<T> &Vec2<T>::operator=(const T &val) noexcept
+template <typename T>
+Vec2<T> &Vec2<T>::operator=(const T val) noexcept
 {
-	x = val;
-	y = val;
-	return *this;
+    x = val;
+    y = val;
+    return *this;
 }
 
-template<typename T>
+template <typename T>
 bool Vec2<T>::operator==(const Vec2<T> &vec) const noexcept
 {
-	return (x == vec.x && y == vec.y);
+    return (x == vec.x && y == vec.y);
 }
 
-template<typename T>
+template <typename T>
 bool Vec2<T>::operator!=(const Vec2<T> &vec) const noexcept
 {
-	return (x != vec.x || y != vec.y);
+    return (x != vec.x || y != vec.y);
 }
 
 using Vec2f = Vec2<float>;
