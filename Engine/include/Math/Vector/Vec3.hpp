c9b34156b61ff76c0620069762f83615e4bf8d3b
byFuuRy <37186269+byFuuRy@users.noreply.github.com>
2019-05-21 13:00:48 +0200

    Add Vec3 (#7)

---
+++ Engine/include/Math/Vector/Vec3.hpp
@@ -0,0 +1,216 @@
+/*
+** EPITECH PROJECT, 2019
+** OOP_indie_studio_2018
+** File description:
+** Vec3
+*/
+
+#pragma once
+
+namespace Engine::Math {
+    class Vec3;
+}
+
+template <typename T>
+class Vec3 {
+public:
+    using Vec3f = Vec3<float>;
+    using Vec3d = Vec3<double>;
+    using Vec3i = Vec3<int>;
+    using Vec3u = Vec3<unsigned int>;
+    using Vec3l = Vec3<long>;
+
+    T x{0};
+    T y{0};
+    T z{0};
+
+    Vec3<T>() noexcept;
+    Vec3<T>(const Vec3<T> &vec) noexcept;
+    Vec3<T>(const T &x, const T &y, const T &z) noexcept;
+    ~Vec3<T>() = default;
+
+    Vec3<T> &operator+=(const Vec3<T> &vec) noexcept;
+    Vec3<T> &operator+=(const T &val) noexcept;
+    Vec3<T> &operator-=(const Vec3<T> &vec) noexcept;
+    Vec3<T> &operator-=(const T &val) noexcept;
+    Vec3<T> &operator*=(const Vec3<T> &vec) noexcept;
+    Vec3<T> &operator*=(const T &val) noexcept;
+    Vec3<T> &operator/=(const Vec3<T> &vec);
+    Vec3<T> &operator/=(const T &val);
+    Vec3<T> operator+(const Vec3<T> &vec) const noexcept;
+    Vec3<T> operator+(const T &val) const noexcept;
+    Vec3<T> operator-(const Vec3<T> &vec) const noexcept;
+    Vec3<T> operator-(const T &val) const noexcept;
+    Vec3<T> operator*(const Vec3<T> &vec) const noexcept;
+    Vec3<T> operator*(const T &val) const noexcept;
+    Vec3<T> operator/(const Vec3<T> &vec) const;
+    Vec3<T> operator/(const T &val) const;
+    Vec3<T> &operator=(const Vec3<T> &vec) noexcept;
+    Vec3<T> &operator=(const T &val) noexcept;
+    bool operator==(const Vec3<T> &vec) const noexcept;
+    bool operator!=(const Vec3<T> &vec) const noexcept;
+};
+
+template <typename T>
+Vec3<T>::Vec3() noexcept
+{}
+
+template <typename T>
+Vec3<T>::Vec3(const Vec3<T> &vec) noexcept
+    : x{vec.x}, y{vec.y}, z{vec.z}
+{}
+
+template <typename T>
+Vec3<T>::Vec3(const T &x, const T &y, const T &z) noexcept
+    : x{x}, y{y}, z{z}
+{}
+
+template <typename T>
+Vec3<T> &Vec3<T>::operator+=(const Vec3<T> &vec) noexcept
+{
+    x += vec.x;
+    y += vec.y;
+    z += vec.z;
+    return *this;
+}
+
+template <typename T>
+Vec3<T> &Vec3<T>::operator+=(const T &val) noexcept
+{
+    x += val;
+    y += val;
+    z += val;
+    return *this;
+}
+
+template <typename T>
+Vec3<T> &Vec3<T>::operator-=(const Vec3<T> &vec) noexcept
+{
+    x -= vec.x;
+    y -= vec.y;
+    z -= vec.z;
+    return *this;
+}
+
+template <typename T>
+Vec3<T> &Vec3<T>::operator-=(const T &val) noexcept
+{
+    x -= val;
+    y -= val;
+    z -= val;
+    return *this;
+}
+
+template <typename T>
+Vec3<T> &Vec3<T>::operator*=(const Vec3<T> &vec) noexcept
+{
+    x *= vec.x;
+    y *= vec.y;
+    z *= vec.z;
+    return *this;
+}
+
+template <typename T>
+Vec3<T> &Vec3<T>::operator*=(const T &val) noexcept
+{
+    x *= val;
+    y *= val;
+    z *= val;
+    return *this;
+}
+
+template <typename T>
+Vec3<T> &Vec3<T>::operator/=(const Vec3<T> &vec)
+{
+    x /= vec.x;
+    y /= vec.y;
+    z /= vec.z;
+    return *this;
+}
+
+template <typename T>
+Vec3<T> &Vec3<T>::operator/=(const T &val)
+{
+    x /= val;
+    y /= val;
+    z /= val;
+    return *this;
+}
+
+template <typename T>
+Vec3<T> Vec3<T>::operator+(const Vec3<T> &vec) const noexcept
+{
+    return Vec3<T>{x + vec.x, y + vec.y, z + vec.z};
+}
+
+template <typename T>
+Vec3<T> Vec3<T>::operator+(const T &val) const noexcept
+{
+    return Vec3<T>{x + val, y + val, z + val};
+}
+
+template <typename T>
+Vec3<T> Vec3<T>::operator-(const Vec3<T> &vec) const noexcept
+{
+    return Vec3<T>{x - vec.x, y - vec.y, z - vec.z};
+}
+
+template <typename T>
+Vec3<T> Vec3<T>::operator-(const T &val) const noexcept
+{
+    return Vec3<T>{x - val, y - val, z - val};
+}
+
+template <typename T>
+Vec3<T> Vec3<T>::operator*(const Vec3<T> &vec) const noexcept
+{
+    return Vec3<T>{x * vec.x, y * vec.y, z * vec.z};
+}
+
+template <typename T>
+Vec3<T> Vec3<T>::operator*(const T &val) const noexcept
+{
+    return Vec3<T>{x * val, y * val, z * val};
+}
+
+template <typename T>
+Vec3<T> Vec3<T>::operator/(const Vec3<T> &vec) const
+{
+    return Vec3<T>{x / vec.x, y / vec.y, z / vec.z};
+}
+
+template <typename T>
+Vec3<T> Vec3<T>::operator/(const T &val) const
+{
+    return Vec3<T>{x / val, y / val, z / val};
+}
+
+template <typename T>
+Vec3<T> &Vec3<T>::operator=(const Vec3<T> &vec) noexcept
+{
+    x = vec.x;
+    y = vec.y;
+    z = vec.z;
+    return *this;
+}
+
+template <typename T>
+Vec3<T> &Vec3<T>::operator=(const T &val) noexcept
+{
+    x = val;
+    y = val;
+    z = val;
+    return *this;
+}
+
+template <typename T>
+bool Vec3<T>::operator==(const Vec3<T> &vec) const noexcept
+{
+    return (x == vec.x && y == vec.y && z == vec.z);
+}
+
+template <typename T>
+bool Vec3<T>::operator!=(const Vec3<T> &vec) const noexcept
+{
+    return !(vec == *this);
+}
