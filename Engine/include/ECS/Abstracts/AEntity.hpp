38322e6fa479d8d2dc70226ddcdbcc9be8181c85
Benjamin Praud <benjamin.praud@epitech.eu>
2019-05-27 16:08:30 +0200

    Fix ID for AEntity

--- Engine/include/ECS/Abstracts/AEntity.hpp
+++ Engine/include/ECS/Abstracts/AEntity.hpp
@@ -19,6 +19,12 @@
 namespace Engine::ECS {
     template <typename T>
     class AEntity;
+
+    static size_t getNextID()
+    {
+        static size_t ID = 0;
+        return ID++;
+    }
 }
 
 template <typename T>
@@ -26,20 +32,23 @@ class Engine::ECS::AEntity : public Engine::ECS::IEntity {
 private:
 
 protected:
-    //static size_t ID;
-
-    //const size_t _id{++ID};
+    const size_t _id;
     std::vector<Engine::ECS::IComponent> _components{};
 
 public:
+    AEntity()
+        : _id{Engine::ECS::getNextID()}
+    {
+    }
+
     ~AEntity() override = default;
 
-    //decltype(_id) id() const noexcept
-    //{
-    //    return _id;
-    //};
+    decltype(_id) getID() const noexcept
+    {
+        return _id;
+    };
 
-    const decltype(_components) &components() const noexcept
+    const decltype(_components) &getComponents() const noexcept
     {
         return _components;
     };
@@ -49,18 +58,17 @@ public:
         _components.push_back(component);
     };
 
-    //void removeComponent()
-    //{
-    //    _components.erase(std::remove(_components.begin(), _components.end(), _id), _components.end());
-    //};
+    void removeComponent()
+    {
+        _components.erase(std::remove(_components.begin(), _components.end(), _id), _components.end());
+    };
 
-    //IComponent &getComponentByID(const size_t ID)
-    //{
-    //    auto search = std::find(_components.begin(), _components.end(), ID);
+    IComponent &getComponentByID(const size_t ID)
+    {
+        auto search = std::find(_components.begin(), _components.end(), ID);
 
-    //    if (search == _components.end())
-    //        throw ECSException<ECS_Entity>("Component unknown");
-    //    return *search;
-    //};
+        if (search == _components.end())
+            throw ECSException<ECS_Entity>("Component unknown");
+        return *search;
+    };
 };
-
