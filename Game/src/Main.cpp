d7dd04a09a7c2f0063171b32683dd2239e8f56df
HOUIS Maxime <maxime.houis@epitech.eu>
2019-06-02 02:31:34 +0200

    Big optimizations

--- Game/src/Main.cpp
+++ Game/src/Main.cpp
@@ -16,7 +16,7 @@
 #include "GameApplication.hpp"
 #include "BombermanApplication.hpp"
 
-class AudioVisualizer {
+class AudioVisualizer final {
 private:
     using Complex = std::complex<double>;
     using ComplexArray = std::valarray<Complex>;
@@ -27,6 +27,7 @@ private:
     const sf::SoundBuffer &_buffer;
 
     ComplexArray _array{};
+    std::array<double, BUFFER_SIZE> _scaleList{};
 
     struct SoundBufferData {
         decltype(_buffer.getChannelCount()) channelCount;
@@ -40,7 +41,44 @@ private:
         {}
     };
 
+    struct VisualizationData {
+        const double scaleAverage;
+        const decltype(_scaleList) *scaleList;
+    };
+
     const SoundBufferData _bufferData;
+
+    void _fastFourierTransform(decltype(_array) &array)
+    {
+        const auto size = array.size();
+
+        if (size < 2)
+            return;
+
+        auto even = decltype(_array)(array[std::slice(0, size / 2, 2)]);
+        auto odd = decltype(_array)(array[std::slice(1, size / 2, 2)]);
+
+        _fastFourierTransform(even);
+        _fastFourierTransform(odd);
+
+        for (decltype(array.size()) i = 0; i < size / 2; ++i) {
+            Complex comp = std::polar(1.0, -2 * M_PI * i / size) * odd[i];
+            array[i] = even[i] + comp;
+            array[i + size / 2] = even[i] - comp;
+        }
+    }
+
+    static void _smoothArray(std::array<double, BUFFER_SIZE> &vec)
+    {
+        for (auto it = vec.begin(); it != vec.end(); ++it) {
+            if (it == vec.begin())
+                *it = (*it + *(it + 1)) / 2.0;
+            else if (it == (vec.end() - 1))
+                *it = (*it + *(it - 1)) / 2.0;
+            else
+                *it = (*(it - 1) + *it + *(it + 1)) / 3.0;
+        }
+    }
 
 public:
     AudioVisualizer(decltype(_sound) sound, decltype(_buffer) buffer)
@@ -54,61 +92,29 @@ public:
     AudioVisualizer(const AudioVisualizer &) = delete;
     AudioVisualizer &operator=(const AudioVisualizer &) = delete;
 
-    void compute(decltype(_array) &array)
+    VisualizationData getVisualizationData()
     {
-        const auto size = array.size();
+        const auto position = static_cast<size_t>(_sound.getPlayingOffset().asSeconds() * _bufferData.sampleRate);
 
-        if (size < 2)
-            return;
-
-        auto even = decltype(_array)(array[std::slice(0, size / 2, 2)]);
-        auto odd = decltype(_array)(array[std::slice(1, size / 2, 2)]);
-
-        compute(even);
-        compute(odd);
-
-        for (decltype(array.size()) i = 0; i < size / 2; ++i) {
-            Complex comp = std::polar(1.0, -2 * M_PI * i / size) * odd[i];
-            array[i] = even[i] + comp;
-            array[i + size / 2] = even[i] - comp;
-        }
-    }
-
-    double getAudioScale()
-    {
-        auto position = static_cast<size_t>(_sound.getPlayingOffset().asSeconds() * _bufferData.sampleRate);
-
-        for (auto i = position; i - position < BUFFER_SIZE && i < _bufferData.sampleCount; ++i) {
+        for (auto i = position; i - position < BUFFER_SIZE && i < _bufferData.sampleCount; ++i)
             _array[i - position] = Complex(_buffer.getSamples()[i], 0.0);
-        }
 
-        compute(_array);
+        _fastFourierTransform(_array);
 
         double sum = 0;
-        for (auto &value : _array)
-            sum += std::abs(value);
 
-        return sum / _array.size() / 50000.0;
+        size_t index = 0;
+        for (auto &value : _array) {
+            const auto abs = std::abs(value);
+
+            sum += abs;
+            _scaleList[index++] = abs;
+        }
+
+        _smoothArray(_scaleList);
+        return VisualizationData{sum / _array.size() / 50000.0, &_scaleList};
     }
 };
-
-template<typename T>
-static std::vector<T> smoothVector(std::vector<T> vec)
-{
-    if (vec.size() < 3)
-        return vec;
-
-    for (auto it = vec.begin(); it != vec.end(); ++it) {
-        if (it == vec.begin())
-            *it = (*it + *(it + 1)) / 2.0;
-        else if (it == vec.end())
-            *it = (*it + *(it - 1)) / 2.0;
-        else
-            *it = (*(it - 1) + *it + *(it + 1)) / 3.0;
-    }
-
-    return vec;
-}
 
 int main()
 {
@@ -123,12 +129,12 @@ int main()
 
     sf::CircleShape circle;
     circle.setFillColor(sf::Color::Yellow);
-    circle.setRadius(50);
-    circle.setOrigin(50, 50);
+    circle.setRadius(25);
+    circle.setOrigin(25, 25);
     circle.setPosition(win.getSize().x / 2.f, win.getSize().y / 2.f);
 
     sf::SoundBuffer buffer{};
-    if (!buffer.loadFromFile("ignite.ogg")) {
+    if (!buffer.loadFromFile("bp.ogg")) {
         std::cerr << "no" << std::endl;
         win.close();
     }
@@ -140,7 +146,6 @@ int main()
 
     AudioVisualizer audioVisualizer{sound, buffer};
 
-    std::vector<double> previous{1000, 0};
     double max = 0;
     while (win.isOpen()) {
 
@@ -150,18 +155,10 @@ int main()
 
         win.clear();
 
-        auto scale = audioVisualizer.getAudioScale();
-        max = std::max(max, scale);
-        auto sum = scale;
+        auto visu = audioVisualizer.getVisualizationData();
+        max = std::max(max, visu.scaleAverage);
 
-        auto smoothed = smoothVector(previous);
-        for (const auto &d : smoothed)
-            sum += d;
-        auto avg = sum / (smoothed.size() + 1) + 0.5;
-
-        circle.setScale(avg, avg);
-        previous.push_back(scale);
-        previous.erase(previous.cbegin());
+        circle.setScale(visu.scaleAverage + 0.5, visu.scaleAverage + 0.5);
 
         win.draw(circle);
         win.display();
