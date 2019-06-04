19bcad2edffd7aa07fcd2389d47c26cf8ef2e59f
clucas <lucas.claisse@epitech.eu>
2019-06-04 16:33:01 +0200

    Init Slider Entity

---
+++ Engine/include/ECS/Components/Slider.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2019
+** bomberman
+** File description:
+** Slider.hpp
+*/
+
+#pragma once
+
+#include <irrlicht/irrlicht.h>
+
+#include "Math/Rect.hpp"
+#include "ECS/Systems/Renderer.hpp"
+#include "ECS/Abstracts/AComponent.hpp"
+
+namespace Engine::ECS::Component {
+    class Slider;
+}
+
+class Engine::ECS::Component::Slider : public Engine::ECS::AComponent<Engine::ECS::Component::Slider> {
+
+};
