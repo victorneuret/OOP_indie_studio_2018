ea28eef54929b45308166bbda1d043a45d4bd064
victorneuret <victor.neuret@epitech.eu>
2019-06-06 14:12:55 +0200

    Apply changes

--- Engine/include/ECS/Components/Model3D.hpp
+++ Engine/include/ECS/Components/Model3D.hpp
@@ -22,7 +22,7 @@ protected:
     irr::scene::IAnimatedMeshSceneNode *_node{nullptr};
     std::string _model{};
     std::string _texture{};
-    uint8_t _textureLayer = 0;
+    uint8_t _textureLayer{0};
 
 public:
     Model3D(const System::Renderer &renderer, const Math::Vec3f &pos, const decltype(_model) &model);
