6cedce0c17c9534b597112c02ad3503b156ae428
HOUIS Maxime <maxime.houis@epitech.eu>
2019-06-12 12:49:25 +0200

    Fix rebase errors

--- Game/src/Scenes/MainMenu.cpp
+++ Game/src/Scenes/MainMenu.cpp
@@ -27,7 +27,7 @@ Game::Scene::MainMenu::MainMenu()
             std::make_shared<Engine::Entity::Image>("assets/img/sun.png", Engine::Math::Vec2u{screenSize.Width / 2 - (894 / 2), 0}),
             std::make_shared<Engine::Entity::Image>("assets/img/mountain.png", Engine::Math::Vec2u{0, 0}),
 
-            std::make_shared<Entity::Button>(
+            std::make_shared<Engine::ECS::Entity::Button>(
                 Engine::Math::Rect_i{
                     static_cast<int>(screenSize.Width * 0.06),
                     static_cast<int>(screenSize.Height - 100),
@@ -37,7 +37,7 @@ Game::Scene::MainMenu::MainMenu()
                 L"New Game",
                 Engine::Utils::Color{255, 0, 0}
             ),
-            std::make_shared<Entity::Button>(
+            std::make_shared<Engine::ECS::Entity::Button>(
                 Engine::Math::Rect_i{
                     static_cast<int>(screenSize.Width * 0.06 * 2 + (screenSize.Width * 0.25)),
                     static_cast<int>(screenSize.Height - 100),
@@ -47,7 +47,7 @@ Game::Scene::MainMenu::MainMenu()
                 L"Load Game",
                 Engine::Utils::Color{255, 0, 0}
             ),
-            std::make_shared<Entity::Button>(
+            std::make_shared<Engine::ECS::Entity::Button>(
                 Engine::Math::Rect_i{
                     static_cast<int>(screenSize.Width * 0.06 * 3 + (screenSize.Width * 0.25 * 2)),
                     static_cast<int>(screenSize.Height - 100),
