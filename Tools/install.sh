e7e87c9cdd093c7fc0c48576f708c46b8d963625
victorneuret <victor.neuret@epitech.eu>
2019-06-14 18:20:03 +0200

    Add unsinstaller
    Change /etc/bomberman permission to 777

--- Tools/install.sh
+++ Tools/install.sh
@@ -6,7 +6,7 @@ sudo mkdir -p /etc/bomberman
 sudo cp bomberman /etc/bomberman
 sudo cp libengine.a /etc/bomberman
 sudo cp -r assets /etc/bomberman
-sudo chmod 755 /etc/bomberman
+sudo chmod 777 /etc/bomberman
 echo -e "#!/bin/sh\n\ncd /etc/bomberman && ./bomberman" > Tools/bomberman
 sudo cp Tools/bomberman /usr/bin
 sudo chmod 755 /usr/bin/bomberman
---
+++ Tools/unsinstall.sh
@@ -0,0 +1,4 @@
+#!/bin/sh -e
+
+sudo rm -rf /etc/bomberman
+sudo rm /usr/bin/bomberman
