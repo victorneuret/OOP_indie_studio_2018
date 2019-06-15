2fe133b2ff04679455fea09b320bfb4a81086b0f
Benjamin Praud <benjamin.praud@epitech.eu>
2019-06-15 16:52:55 +0200

    Fix powerup texture

--- Game/include/Entities/PowerUp.hpp
+++ Game/include/Entities/PowerUp.hpp
@@ -15,7 +15,7 @@ namespace Game::Entity {
 
 class Game::Entity::PowerUp : public Engine::ECS::AEntity {
 public:
-    PowerUp(const Engine::Math::Vec3f &pos, const std::string &model = std::string("assets/models/cube.obj"));
+    PowerUp(const Engine::Math::Vec3f &pos, const std::string &model = std::string("assets/models/block/powerup.obj"));
 
     PowerUp(const PowerUp &) = delete;
     PowerUp &operator=(const PowerUp &) = delete;
--- Game/src/Entities/PowerUp.cpp
+++ Game/src/Entities/PowerUp.cpp
@@ -14,7 +14,7 @@ Game::Entity::PowerUp::PowerUp(const Engine::Math::Vec3f &pos, const std::string
     addComponent(_3DModel);
 
     std::dynamic_pointer_cast<Engine::ECS::Component::Model3D>(_3DModel)->addTexture("assets/img/Bombupsprite.png");
-    std::dynamic_pointer_cast<Engine::ECS::Component::Model3D>(_3DModel)->setScale(Engine::Math::Vec3f{0.03, 0.03, 0.03});
+    std::dynamic_pointer_cast<Engine::ECS::Component::Model3D>(_3DModel)->setScale(Engine::Math::Vec3f{5, 5, 5});
 
     std::shared_ptr<Engine::ECS::IComponent> _Renderer = std::make_shared<Engine::ECS::Component::Renderer>();
     addComponent(_Renderer);
