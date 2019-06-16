2191fb8737e2197286b04f426cdca3e6b5611f8d
shellbear <antoine.ordonez@epitech.eu>
2019-06-16 16:00:57 +0200

    Clean code

--- Game/src/Entities/Player.cpp
+++ Game/src/Entities/Player.cpp
@@ -6,14 +6,9 @@
 */
 
 #include <memory>
-#include <filesystem>
 
-#include "Systems/Map.hpp"
 #include "Entities/Player.hpp"
 #include "Math/Vector/Vec3.hpp"
-#include "ECS/Components/Model3D.hpp"
-#include "ECS/Interfaces/IEntity.hpp"
-#include "ECS/Components/Renderer.hpp"
 
 Game::Entity::Player::Player(const Engine::Math::Vec3f &pos, const std::string &texture, const std::string &model)
     : Character{pos, texture, model}
